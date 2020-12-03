4335ea1a90d54d43743a4f17d99bd20d76f86f0a
pascalbaljet <pascal@pascalbaljet.nl>
2020-12-03 13:42:38 +0100

    refactor

---
+++ resources/views/postRow.blade.php
@@ -0,0 +1,9 @@
+<tr class="@if($loop->even) bg-gray-200 @endif">
+    <td class="border px-4 py-2">Post #{{ $post->id }} </td>
+    <td class="border px-4 py-2 w-1/2">
+        <p>{{ $post->title }}</p>
+        <p class="text-xs">{{ $post->subtitle }}</p>
+    </td>
+    <td class="border px-4 py-2">{{ $post->published_at }}</td>
+    <td class="border px-4 py-2">{{ $post->comments_count }} {{ Str::plural('comment', $post->comments_count) }}</td>
+</tr>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -23,15 +23,7 @@
 
                     <tbody>
                         @foreach($posts as $post)
-                            <tr class="@if($loop->even) bg-gray-200 @endif">
-                                <td class="border px-4 py-2">Post #{{ $post->id }} </td>
-                                <td class="border px-4 py-2 w-1/2">
-                                    <p>{{ $post->title }}</p>
-                                    <p class="text-xs">{{ $post->subtitle }}</p>
-                                </td>
-                                <td class="border px-4 py-2">{{ $post->published_at }}</td>
-                                <td class="border px-4 py-2">{{ $post->comments_count }} {{ Str::plural('comment', $post->comments_count) }}</td>
-                            </tr>
+                            @include('postRow')
                         @endforeach
                     </tbody>
                 </table>
